30f1b9c3cdfcc392e74a97c53b1ba00d1ba78a47
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-08-13 22:36:32 +0200

    lec 9 optional parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Posts = () => {
+const Posts = ({match}) => {
+  console.log(match);
   return (
     <div>
       <h1>Posts</h1>
-      Year: , Month:
+      Year: {match.params.year}, Month: {match.params.month}:
     </div>
   );
 };
