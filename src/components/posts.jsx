778a15312383ef6f8c200c1026464a4ab5e8bb3e
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-08-14 00:22:53 +0200

    lec 10 query string parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-
-const Posts = ({match}) => {
-  console.log(match);
+import React from 'react'
+import queryString from 'query-string'
+const Posts = ({ match, location }) => {
+ const result= queryString.parse(location.search)
+console.log(result);
   return (
     <div>
       <h1>Posts</h1>
       Year: {match.params.year}, Month: {match.params.month}:
     </div>
-  );
-};
+  )
+}
 
-export default Posts;
+export default Posts
